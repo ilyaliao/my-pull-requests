70ea35121f59d337460f3004e609362f768fe43a
IlyaL <ilyaliao324@gmail.com>
2025-08-27 23:18:28 +0800

    feat: deploy at zeabur

--- app/app.vue
+++ app/app.vue
@@ -9,30 +9,19 @@ useHead({
 <template>
   <UApp>
     <NuxtPage />
-    <p class="py-6 pb-10 text-sm flex items-center justify-center gap-2 flex-wrap px-4">
+    <div class="flex flex-wrap justify-center items-center gap-2 p-4">
+      <span class="text-sm text-gray-600 dark:text-gray-400">Deployed at</span>
       <UButton
-        to="https://github.com/atinux/my-pull-requests"
-        rel="noopener"
+        to="https://zeabur.com"
         target="_blank"
-        color="neutral"
-        variant="subtle"
-        size="sm"
-        icon="i-simple-icons-github"
+        variant="ghost"
+        size="xs"
+        color="violet"
+        icon="i-material-icon-theme:zeabur-light"
       >
-        Source code
+        Zeabur
       </UButton>
-      <UButton
-        to="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fatinux%2Fmy-pull-requests&env=NUXT_GITHUB_TOKEN&envDescription=Create%20a%20GitHub%20token%20with%20no%20special%20scope.&envLink=https%3A%2F%2Fgithub.com%2Fsettings%2Fpersonal-access-tokens%2Fnew&project-name=my-pull-requests&demo-title=My%20Pull%20Requests&demo-description=Create%20a%20website%20with%20an%20RSS%20feed%20of%20your%20recent%20GitHub%20pull%20requests%20across%20the%20Open%20Source%20projects%20you%20contribute%20to.&demo-url=https%3A%2F%2Fprs.atinux.com&demo-image=https%3A%2F%2Fprs.atinux.com%2Fog.png"
-        target="_blank"
-        rel="noopener"
-        color="neutral"
-        variant="solid"
-        size="sm"
-        icon="i-simple-icons-vercel"
-      >
-        Deploy with Vercel
-      </UButton>
-    </p>
+    </div>
     <ScrollToTop />
   </UApp>
 </template>
